526cb9991de6be18b3f127bee8a80afd2cd2d4c3
JuanMiguelGM <esqizo@gmail.com>
2017-09-24 17:43:49 +0200

    removed all helper method

--- app/helpers/meals_helper.rb
+++ app/helpers/meals_helper.rb
@@ -1,14 +1,5 @@
 module MealsHelper
 
-  def link_to_show_or_add_meal kind, date
-    meal = Meal.find_by_date_and_kind(date, Meal.kinds[kind.to_sym])
-    if meal
-      link_to meal.recipe.name, edit_meal_path(meal)
-    else
-      link_to kind, new_meal_path(date: date, kind: kind)
-    end
-  end
-
   def lunch_for date
     Meal.where(date: date, kind: :lunch)
   end
